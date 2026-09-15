2fda177526522046b841912fdd81e5e074443558
Nawaf <alserhaninawaf@gmail.com>
2026-09-15 21:57:08 +0300

    final api url update

--- api.py
+++ api.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://grounded-web-lovat.vercel.app",
+        "https://grounded-poeajmkww-nawafalsharani-1520s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
